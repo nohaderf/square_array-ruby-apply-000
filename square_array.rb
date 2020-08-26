25cf51e1082f482a2ffb01810ecc9f2e1b415732
Freda Hon <freda.hon@gmail.com>
2020-08-26 03:59:48 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -11,11 +11,3 @@ end
 # a new array needs to be made, and the sqaure root of each element needs to be pushed into the array
 # numbers_square.push(number ** 2) didn't work
 
-
-
-def square_array(array)
-  numbers_square = [ ]
-  array.each do |number|
-    numbers_square << (number ** 2)
-  end
-end
