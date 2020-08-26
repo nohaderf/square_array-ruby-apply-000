c18974cb01c198ac700b23a43dfccc1cf93c688c
Freda Hon <freda.hon@gmail.com>
2020-08-26 04:02:45 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  new_numbers = [ ]
+  array.each do |numbers|
+    new_numbers.push(numbers ** 2)
+  end
+  return new_numbers
 end
+
+
+# exponent operator = **
+# a new array needs to be made, and the sqaure root of each element needs to be pushed into the array
+
