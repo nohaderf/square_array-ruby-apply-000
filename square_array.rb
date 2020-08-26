3c104f47a08a698ba3e4571afb51321b7e5c4237
Freda Hon <freda.hon@gmail.com>
2020-08-26 04:02:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -9,5 +9,4 @@ end
 
 # exponent operator = **
 # a new array needs to be made, and the sqaure root of each element needs to be pushed into the array
-# numbers_square.push(number ** 2) didn't work
 
