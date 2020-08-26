6ad6a48685b59e9af16759bc332d7e457e052f6f
Freda Hon <freda.hon@gmail.com>
2020-08-26 03:58:24 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
-def square_array(array)
-  numbers_square = [ ]
-  array.each do |number|
-    numbers_square << (number ** 2)
+def square_array(numbers)
+  new_numbers = [ ]
+  numbers.each do |num|
+    new_numbers << (num ** 2)
   end
+  return new_numbers
 end
 
 
