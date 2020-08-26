13a8b742f1d0008745bf108ac64fb60404791999
Freda Hon <freda.hon@gmail.com>
2020-08-26 03:44:38 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,10 @@
 def square_array(array)
   numbers_square = [ ]
-  numbers.each { |number|**2 }
+  numbers.each do |number|
+    (number ** 2).push
   end
 end
+
+
+# exponent operator = **
+# a new array needs to be made, and the sqaure root of each element needs to be pushed into the array
